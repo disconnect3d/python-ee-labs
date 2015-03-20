ee16b7e2b9c4dbf88f40a5a4dc12d12abeb01cf0
disconnect3d <noname@noname.noname>
2015-03-20 18:45:50 +0100

    Added lab2: Implementation made on lab; lacks checking winner

---
+++ lab2/utils.py
@@ -0,0 +1,13 @@
+import logging
+
+# timestamp, module, log msg
+FORMAT = '%(asctime)-10s %(module)s-10s %(funcName)-10s %(message)s'
+logging.basicConfig(format=FORMAT)
+
+
+def logger_injector(cls):
+    def wrap(*args, **kwargs):
+        cls.logger = logging.getLogger(cls.__name__)
+        return cls(*args, **kwargs)
+
+    return wrap
